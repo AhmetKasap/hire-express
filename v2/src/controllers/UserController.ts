89f6217c8860a3f15ca506ef38eeb65dee93b964
ahmetkasap <aahmetkasap@gmail.com>
2024-12-10 13:56:29 +0300

    v2 response structure created

--- v2/src/controllers/UserController.ts
+++ v2/src/controllers/UserController.ts
@@ -2,28 +2,21 @@ import { inject, injectable } from "inversify";
 import TYPES from "../inversify/types";
 import { IUserService } from "../services/interfaces/IUserService";
 import { Request, Response } from "express";
+import APIResponse from "../shared/utils/APIResponse";
 
 @injectable()
 export class UserController {
     constructor(@inject(TYPES.UserService) private readonly userService: IUserService) {}
 
     async getAllUsers(req: Request, res: Response): Promise<void> {
-        try {
-            const users = await this.userService.getAllUsers();
-            res.json(users);
-        } catch (error) {
-            res.status(500).json("error");
-        }
+        const users = await this.userService.getAllUsers();
+        new APIResponse('ok', users).ok(res)
     }
 
-
-    async createUser(req : Request, res : Response) : Promise <void> {
-        const {name, email, password} = req.body
-        const registerDTO = {name,email,password}
-         
-        const newUser = await this.userService.createUser(registerDTO)
+    async createUser(req: Request, res: Response): Promise<void> {
+        const { name, email, password } = req.body;
+        const registerDTO = { name, email, password };
+        const newUser = await this.userService.createUser(registerDTO);
+        new APIResponse("User created successfully", newUser).created(res);
     }
-
-
-    
 }
